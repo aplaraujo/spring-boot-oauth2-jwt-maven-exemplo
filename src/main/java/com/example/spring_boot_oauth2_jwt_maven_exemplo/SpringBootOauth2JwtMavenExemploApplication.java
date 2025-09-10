b804feac7b48e6d2b5391c18d3b9522123757093
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-10 18:42:04 -0300

    first commit

---
+++ src/main/java/com/example/spring_boot_oauth2_jwt_maven_exemplo/SpringBootOauth2JwtMavenExemploApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_oauth2_jwt_maven_exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2JwtMavenExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2JwtMavenExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_oauth2_jwt_maven_exemplo/SpringBootOauth2JwtMavenExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_oauth2_jwt_maven_exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2JwtMavenExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
