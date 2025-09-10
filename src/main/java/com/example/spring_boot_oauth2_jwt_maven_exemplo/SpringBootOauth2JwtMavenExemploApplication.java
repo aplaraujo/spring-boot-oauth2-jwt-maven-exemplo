3bc5a7f1b7f044bc9e1fed3bfbddb345a757f4ce
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-10 20:12:23 -0300

    BCrypt password

--- src/main/java/com/example/spring_boot_oauth2_jwt_maven_exemplo/SpringBootOauth2JwtMavenExemploApplication.java
+++ src/main/java/com/example/spring_boot_oauth2_jwt_maven_exemplo/SpringBootOauth2JwtMavenExemploApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOauth2JwtMavenExemploApplication {
+public class SpringBootOauth2JwtMavenExemploApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOauth2JwtMavenExemploApplication.class, args);
